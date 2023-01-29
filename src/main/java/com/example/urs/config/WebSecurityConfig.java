a6e456229f673ae00ae543899c5452898ead8338
Tin Tadić <tin.tadic46@gmail.com>
2023-01-29 18:11:58 +0100

    add 1 user

--- src/main/java/com/example/urs/config/WebSecurityConfig.java
+++ src/main/java/com/example/urs/config/WebSecurityConfig.java
@@ -31,6 +31,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
+                .antMatchers("/user/add").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin().permitAll()
